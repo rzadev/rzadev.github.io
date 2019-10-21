a3083dee3458943b31fc3481973ccdb27e916ac1
creative301 <rza9001@gmail.com>
2019-10-21 22:32:48 +0700

    Add restaurant reviews reactjs

---
+++ restaurant_reviews_reactjs/precache-manifest.295c975d912ce9d77f8ff8f4b17456b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de7f7434b9d8a10ce6799f31e3ac747",
+    "url": "/restaurant_reviews_reactjs/index.html"
+  },
+  {
+    "revision": "19cb5b8f74ace76a1f83",
+    "url": "/restaurant_reviews_reactjs/static/css/main.74462df2.chunk.css"
+  },
+  {
+    "revision": "1d778b5afa6af885e7a1",
+    "url": "/restaurant_reviews_reactjs/static/js/2.730fe551.chunk.js"
+  },
+  {
+    "revision": "19cb5b8f74ace76a1f83",
+    "url": "/restaurant_reviews_reactjs/static/js/main.bcb415c8.chunk.js"
+  },
+  {
+    "revision": "3b99dc083ea4242ad0ca",
+    "url": "/restaurant_reviews_reactjs/static/js/runtime~main.25f7223f.js"
+  }
+]);
